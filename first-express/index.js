1db641b26882d3b3aed889b50fc450ff2708cc4e
pasukanbocil <ctian8787@gmail.com>
2024-03-14 15:31:36 +0700

    docs: :lock: Amelia

--- first-express/index.js
+++ first-express/index.js
@@ -1,9 +1,29 @@
 const express = require("express");
 const app = express();
 
-app.use((req, res) => {
-  console.log("we got request");
-  res.send(`ini string dari respon`);
+// app.use((req, res) => {
+//   console.log("we got request");
+//   res.send(`ini string dari respon`);
+// });
+
+app.get("/", (req, res) => {
+  res.send("This is home page");
+});
+
+app.get("/cats", (req, res) => {
+  res.send("This is cats page");
+});
+
+app.post("/cats", (req, res) => {
+  res.send("this is cats page from post");
+});
+
+app.get("/about", (req, res) => {
+  res.send("This is about page");
+});
+
+app.get("*", (req, res) => {
+  res.send("Halaman Tidak Ditemukan");
 });
 
 app.listen(8080, () => {
