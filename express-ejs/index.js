b1d744fdd96666b52f05194ce5e411c7c68abf4d
pasukanbocil <ctian8787@gmail.com>
2024-03-15 15:05:46 +0700

    docs: :closed_lock_with_key: M

--- express-ejs/index.js
+++ express-ejs/index.js
@@ -1,7 +1,9 @@
+const path = require("path");
 const express = require("express");
 const app = express();
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
 app.get("/", (req, res) => {
   res.render("home");
