d1424946b19a706ae8a6562120c290e9087149ce
pasukanbocil <ctian8787@gmail.com>
2024-03-16 13:52:21 +0700

    docs: :closed_lock_with_key: Parsing Data EJS

--- express-ejs/index.js
+++ express-ejs/index.js
@@ -9,6 +9,11 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
+app.get("/rand", (req, res) => {
+  const num = Math.floor(Math.random() * 10) + 1;
+  res.render("random", { num });
+});
+
 app.listen(8080, () => {
   console.log(`listening on host http://localhost:8080`);
 });
